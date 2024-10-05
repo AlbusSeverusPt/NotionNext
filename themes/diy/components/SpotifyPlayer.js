288e20f7b7001044752ee43960aecf7fccac3bc8
MuJjiang <1061336952@qq.com>
2024-10-05 22:06:06 +0800

    按钮展开功能测试3

---
+++ themes/diy/components/SpotifyPlayer.js
@@ -0,0 +1,41 @@
+import { siteConfig } from '@/lib/config'
+import { useGlobal } from '@/lib/global'
+import CONFIG from '../config'
+
+/**
+ * 跳转到网页顶部
+ * 当屏幕下滑500像素后会出现该控件
+ * @param targetRef 关联高度的目标html标签
+ * @param showPercent 是否显示百分比
+ * @returns {JSX.Element}
+ * @constructor
+ */
+const SpotifyPlayer = ({ showPercent = true, percent }) => {
+  const { locale } = useGlobal()
+  const [isIframeVisible, setIsIframeVisible] = useState(false)
+  const toggleIframeVisibility = () => {
+    setIsIframeVisible(!isIframeVisible)
+  }
+  
+//   以后考虑是否要加上这个
+//   if (!siteConfig('HEXO_WIDGET_TO_TOP', null, CONFIG)) {
+//     return <></>
+//   }
+
+return (
+    <div className='relative space-x-1 items-center justify-center transform hover:scale-105 duration-200 w-7 h-auto pb-1 text-center' onClick={toggleIframeVisibility} >
+      <div title={locale.POST.TOP} ><i className='fas fa-arrow-up text-xs' /></div>
+      {isIframeVisible && (
+        <iframe
+          src="https://example.com"
+          title="example iframe"
+          className="absolute top-0 left-0"
+          style={{ display: isIframeVisible ? 'block' : 'none' }}
+        ></iframe>
+      )}
+    </div>
+  )
+
+}
+
+export default SpotifyPlayer
